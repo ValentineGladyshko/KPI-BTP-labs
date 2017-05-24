867a467b20cc6f3a58adc7c114adc51460341d15
ValentineGladyshko <valentine9022@gmail.com>
2017-05-24 11:55:31 +0300

    Created class Point

--- lab1/lab1/Program.cs
+++ lab1/lab1/Program.cs
@@ -8,8 +8,52 @@ namespace lab1
 {
     class Program
     {
+        class Point
+        {
+            double x;
+            double y;
+
+            public Point(double x, double y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+
+            public Point()
+            {
+                x = 0;
+                y = 0;
+            }
+
+            public double GetX()
+            {
+                return x;
+            }
+
+            public double GetY()
+            {
+                return y;
+            }
+
+            public void SetX(double x)
+            {
+                this.x = x;
+            }
+
+            public void SetY(double y)
+            {
+                this.y = y;
+            }
+        }
+
+        double GetDistance(Point point1, Point point2)
+        {
+            return Math.Sqrt(Math.Pow( (point1.GetX() - point2.GetX()), 2) + Math.Pow((point1.GetY() - point2.GetY()), 2));
+        }
+
         static void Main(string[] args)
         {
+            Point a = new Point(1, 1);
             Console.WriteLine("Hello world!!!");
             Console.ReadKey();
         }
